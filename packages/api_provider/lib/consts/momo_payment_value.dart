c837bdbb506fe6b50408e84a41a451699b20ecdd
Lam Nguyen <nguyenlamzx@gmai.com>
2021-08-20 07:19:38 +0700

    update docs

--- packages/api_provider/lib/consts/momo_payment_value.dart
+++
@@ -1,8 +0,0 @@
-abstract class DefinedMomoPaymentValue {
-  static const merchantCode = 'MOMOI7R020210402';
-  static const merchantName = 'CÔNG TY CỔ PHẦN TẬP ĐOÀN MCV';
-  static const merchantNameLabel = 'CÔNG TY CỔ PHẦN TẬP ĐOÀN MCV';
-  static const appScheme = 'momoi7r020210402';
-  static const action = 'gettoken';
-  static const queryScheme = 'momo';
-}
