f0a1f1b2d541407f60dc51fe47ba11b8eb0a9029
Lam Nguyen <nguyenlamzx@gmail.com>
2022-09-19 03:08:14 +0700

    chore: clean flux_widgets

--- packages/flux_widgets/lib/app/view/app.dart
+++ packages/flux_widgets/lib/app/view/app.dart
@@ -1,14 +1,5 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flux_widgets/counter/counter.dart';
-import 'package:flux_widgets/l10n/l10n.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -22,11 +13,6 @@ class App extends StatelessWidget {
           accentColor: const Color(0xFF13B9FF),
         ),
       ),
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-      ],
-      supportedLocales: AppLocalizations.supportedLocales,
       home: const CounterPage(),
     );
   }
--- packages/flux_widgets/lib/counter/view/counter_page.dart
+++ packages/flux_widgets/lib/counter/view/counter_page.dart
@@ -1,14 +1,6 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flux_widgets/counter/counter.dart';
-import 'package:flux_widgets/l10n/l10n.dart';
 
 class CounterPage extends StatelessWidget {
   const CounterPage({super.key});
@@ -27,9 +19,8 @@ class CounterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final l10n = context.l10n;
     return Scaffold(
-      appBar: AppBar(title: Text(l10n.counterAppBarTitle)),
+      appBar: AppBar(),
       body: const Center(child: CounterText()),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
