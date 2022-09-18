797735ad1b90089d85026eca31a10b748fe75f35
Lam Nguyen <nguyenlamzx@gmail.com>
2022-09-19 03:44:51 +0700

    chore: remove unused code

--- packages/flux_widgets/android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.verygoodcore.flux_widgets
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ packages/flux_widgets/lib/src/fx_card.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+/// A card that displays the current value of a counter.
+class FxCard extends StatelessWidget {
+  const FxCard({
+    super.key,
+    required this.children,
+  });
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: ContinuousRectangleBorder(
+        borderRadius: BorderRadius.circular(48),
+      ),
+      elevation: 3,
+      child: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '2',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
