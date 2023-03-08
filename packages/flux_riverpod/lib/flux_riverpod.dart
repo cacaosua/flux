aa7b0841ad633a3627eb44300b54d266277254e1
Lam Nguyen <nguyenlamzx@gmail.com>
2023-03-09 05:02:26 +0700

    feat: update deps for flux_riverpod

--- packages/flux_riverpod/lib/flux_riverpod.dart
+++ packages/flux_riverpod/lib/flux_riverpod.dart
@@ -1,5 +1,6 @@
 export 'package:flutter_riverpod/flutter_riverpod.dart';
 export 'package:hooks_riverpod/hooks_riverpod.dart';
+export 'package:riverpod_annotation/src/riverpod_annotation.dart';
 
 class FluxRiverpod {
   FluxRiverpod();
