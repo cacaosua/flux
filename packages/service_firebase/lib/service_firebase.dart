016e5b5538a31f92a67e91b4c6a37ebaf6ebcd42
Lam Nguyen <nguyenlamzx@gmail.com>
2023-03-09 05:01:53 +0700

    feat: add service_firebase

---
+++ packages/service_firebase/lib/service_firebase.dart
@@ -0,0 +1,10 @@
+export 'package:firebase_analytics/firebase_analytics.dart';
+export 'package:firebase_core/firebase_core.dart';
+export 'package:firebase_crashlytics/firebase_crashlytics.dart';
+export 'package:firebase_messaging/firebase_messaging.dart';
+export 'package:firebase_performance/firebase_performance.dart';
+export 'package:firebase_remote_config/firebase_remote_config.dart';
+
+class ServiceFirebase {
+  ServiceFirebase();
+}
